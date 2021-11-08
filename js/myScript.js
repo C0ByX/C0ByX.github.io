e193549aee6c83e5cd81c240f3670dd02bab7285
C0ByX <91337977+C0ByX@users.noreply.github.com>
2021-11-08 12:26:47 +0300

    Add files via upload

--- js/myScript.js
+++ js/myScript.js
@@ -1,87 +1,110 @@
 "use strict"
 
-alert("Привет! Сейчас будет калькулятор!!!");
-let clin = [0, 0, 0];
-let i = 0,
-    p = 0,
-	t = 0;
-
-function collect() {
-while (clin[i] <= 0 || clin[i] > 4) {
-	clin[i] = prompt("Тип сайта: 1 - Сайт-визитка, 2 - Корпоративный, 3 - Интернет магазин, 4 - Лэндинг");
-}
-i++;
-
-while (clin[i] <= 0 || clin[i] > 4) {
-	clin[i] = prompt("Дизайн сайта: 1 - Минимализм, 2 - Корпоративный, 3 - Информационный, 4 - Классический");
-}
-i++;
-
-while (clin[i] <= 0 || clin[i] > 3) {
-	clin[i] = prompt("Адаптивность: 1 - Адаптивный, 2 - Только ПК, 3 - Только телефоны");
-}
-
-
-}
-    function priceType () {
-	if (clin[1] == 1){
-		p += 1000;
-		t += 3;
-	}
-	else if (clin[1] == 2){
-		p += 2000;
-		t += 5;
-	}
-	else if (clin[1] == 3){
-		p += 3000;
-		t += 14;
-	}
-	else if (clin[1] == 4){
-		p += 4000;
-		t +=4;
-	}
-	}
-	function priceDesign () {
-		if (clin[2] == 1){
-		p += 1000;
-		t += 1;
-	}
-	else if (clin[2] == 2){
-		p += 1500;
-		t += 3;
-	}
-	else if (clin[2] == 3){
-		p += 2000;
-		t += 4;
-	}
-	else if (clin[2] == 4){
-		p += 1750;
-		t +=2;
-	}
+$(document).ready(function(){
+	$('a[href^="#"]').click(function(){
+		let valHref = $(this).attr("href");
+		$('html, body').animate({scrollTop: $(valHref).offset().top - 40 + "px"})
+	});
+	$(window).scroll(() => {
+		let scrollDistance = $(window).scrollTop();
 		
-	}
-	function priceAdapt () {
-		if (clin[3] == 1){
-		p += 2000;
-		t += 1;
-	}
-	else if (clin[3] == 2){
-		p += 1000;
-		t += 0;
-	}
-	else if (clin[3] == 3){
-		p += 1000;
-		t += 0;
-	}
-	}
+		$(".section").each((i, el) => {
+			if($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
+				$("nav a").each((i, el) => {
+					if($(el).hasClass("activeA")){
+						$(el).removeClass("activeA");
+					}	
+				});
+				$('nav li:eq('+ i +')').find('a').addClass('activeA');
+			}
+		});
+	});
+	
+	function animNum(minChis, maxChis, elem, durAnim){
+			
+			$({numberValue: minChis}).animate({numberValue: maxChis}, {
+		
+			duration: durAnim,
+			easing: "swing",
+			
+			step: function(val) {
+			
+				$(elem).html(Math.ceil(val));
+				
+			}
+			});	
+		}
+	function animStat(){
+			animNum(0, 120, '#kli', 2500);
+			animNum(0, 4600, '#scha', 2500);
+			animNum(0, 340, '#tijo', 2500);
+			animNum(0, 23, '#awta', 2500);
+		}
+	
+	$('#raschetBut').click(function rasch(){
+		let type = $('select[name=tip]').val(),
+            dezi = $('select[name=diz]').val(),
+            adap = $('select[name=adapt]').val(),
+			stoimInfo =[ [1000, 2000, 3000, 4000,1000, 1375, 1950, 2300,2000, 1000, 1000],
+			             [3, 5, 14, 4, 1, 3, 4, 2, 1, 0, 0]];
+						 
+		let	stoim = stoimInfo[0][type] + stoimInfo[0][dezi] + stoimInfo[0][adap],
+			srok = stoimInfo[1][type] + stoimInfo[1][dezi] + stoimInfo[1][adap];
 
+		animNum(0, srok, '#sr', 1000);
+		animNum(0, stoim, '#otv1', 1000);
+	});
+	let oneAni = 0;
+	function onEntry (entry){
+		entry.forEach(change => {
+			if(change.isIntersecting && oneAni == 0){
+				animStat();
+				oneAni++;
+			}
+		})
+	}
+	function otlAnim(){
+		let options = {threshold: [0.5]};
+		let observer = new IntersectionObserver(onEntry, options);
+		let elements = $('.element-animation');
+		elements.each((i, el) => {
+			observer.observe(el);
+		});
+	}
+	function loadPic(){
+		let options = {threshold: [0.7]};
+		let observer = new IntersectionObserver(onPic, options);
+		let elements = $('.load-pic');
+		elements.each((i, el) => {
+			observer.observe(el);
+		});
+	}
+	function onPic (entry){
+		let i = 1;
+		entry.forEach(change => {
+			if(change.isIntersecting && oneAni == 0){
+				while(i != 5){
+					let a = $('.pic'+i).attr('data-src'+i);
+					$('.pic'+i).attr('src', a);
+					i++;
+				}
+				}
+				});
+			}
+	loadPic();
+	otlAnim();
+	$('.image-link').magnificPopup({type:'image'});
+	$('.karots').slick({
+		infinite: true,
+        dots: true,
+		slidesToShow: 1,
+	  });
+	  $('.wintime').hide(0).css('opacity','1');
+	  setTimeout(function(){
+		  $('.wintime').fadeIn(500, 'linear');
+	  },5000);
+	  $('#closeWin').click(function(){
+		  $('.wintime').fadeOut('normal','linear');
+	  })
+});
 
-function price (){
-	priceType();
-	priceDesign();
-	priceAdapt();
-}
-
-collect();
-price();
-alert('Примерная цена: '+(p)+'р. '+'Примерные сроки: '+(t)+' дня.');
